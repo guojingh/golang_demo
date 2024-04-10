43bf9481889e64dfa17a265751e53a47b29e2a64
ahu <1774747992@qq.com>
2024-04-10 17:54:52 +0800

    feat: 配置 GOMAXPROCS 联系

---
+++ examples/module1/gmp/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+// 获取 GOMAXPROCS GMP 模型中的 P(调度器) 的数量就是通过这个设置的
+func getGOMAXPROCS() (int, int) {
+	//_ := runtime.NumCPU()
+	runtime.ThreadCreateProfile()
+	return runtime.GOMAXPROCS(0), runtime.NumCPU()
+}
+
+func main() {
+	p, c := getGOMAXPROCS()
+	fmt.Printf("GOMAXPROCS: %d; NumCPU: %d\n", p, c)
+}

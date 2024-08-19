fcc707ae0a78e66d3e79fef36c85391403fb0b2e
gjh <1098@qq.com>
2024-08-19 08:38:40 +0800

    day03 前缀树router

---
+++ gee/day3-router/gee/tire.go
@@ -0,0 +1,16 @@
+package gee
+
+import (
+	"fmt"
+)
+
+type node struct {
+	pattern  string
+	part     string
+	children []*node
+	isWild   bool
+}
+
+func (n *node) String() string {
+	return fmt.Sprintf("node{pattern=%s, part=%s, isWild=%t}", n.pattern, n.part, n.isWild)
+}
